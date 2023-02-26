9d738280675e0deda1aef74969c193d5bcbca5e8
Alois Sečkár <ellrohir@seznam.cz>
2023-02-26 17:09:43 +0100

    fixed item selection

--- components/ItemSelect.vue
+++ components/ItemSelect.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
-    <FormKit type="form" submit-label="Edit" @submit="$emit('selectItem', item)">
-      <FormKit
-        id="selectItem"
-        v-model="item"
-        type="select"
-        name="selectItem"
-        label="Vyberte existující:"
-        validation="required"
-        :options="options"
-      />
-    </FormKit>
+    <FormKit
+      id="selectItem"
+      v-model="item"
+      type="select"
+      name="selectItem"
+      label="Vyberte existující:"
+      validation="required"
+      :options="options"
+      @change="$emit('selectItem', item)"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { FormKitOptionsItem } from '@formkit/inputs'
 import type { WolfItem } from '@/composables/useItemStore'
 
-const options: FormKitOptionsItem[] = useItemStore().items.map((i: WolfItem) => { return { value: i.id, label: i.description } })
+const options = computed(() => {
+  const items = useItemStore().items.map((i: WolfItem) => { return { value: i.id, label: i.description } })
+  items.unshift({ value: -1, label: '(založit nový předmět)' })
+  return items
+})
 const item = ref(-1)
 
 defineEmits<{(e: 'selectItem', option: String): void}>()
