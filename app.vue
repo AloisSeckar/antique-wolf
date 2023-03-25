b25b5d8625b0bb060f36d1b506e62b5c677782ad
Alois Sečkár <ellrohir@seznam.cz>
2023-03-25 12:10:51 +0100

    footer - link na index z administrace

--- app.vue
+++ app.vue
@@ -5,21 +5,7 @@
       <h1>Jaroslav Wolf, Antikvariát Hobby</h1>
     </NuxtLink>
     <NuxtPage />
-    <footer class="mt-8 text-xs text-white">
-      <NuxtLink to="http://alois-seckar.cz" :external="true" class="mr-1 hover:text-amber-200">
-        Web by Alois Sečkár
-      </NuxtLink>
-      |
-      <NuxtLink to="https://www.vecteezy.com/free-vector/wolf" :external="true" class="ml-1 hover:text-amber-200">
-        Wolf by Vecteezy
-      </NuxtLink>
-      |
-      <NuxtLink to="/admin/items">
-        <div class="inline ml-1 text-xs text-white hover:text-amber-200" title="Administrace">
-          A
-        </div>
-      </NuxtLink>
-    </footer>
+    <TheFooter />
   </div>
 </template>
 
---
+++ components/TheFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="mt-8 text-xs text-white">
+    <NuxtLink to="http://alois-seckar.cz" :external="true" class="mr-1 hover:text-amber-200">
+      Web by Alois Sečkár
+    </NuxtLink>
+    |
+    <NuxtLink to="https://www.vecteezy.com/free-vector/wolf" :external="true" class="ml-1 hover:text-amber-200">
+      Wolf by Vecteezy
+    </NuxtLink>
+    |
+    <NuxtLink v-if="useRoute().path === '/admin/items'" to="/">
+      <div class="inline ml-1 text-xs text-white hover:text-amber-200" title="Index">
+        Index
+      </div>
+    </NuxtLink>
+    <NuxtLink v-else to="/admin/items">
+      <div class="inline ml-1 text-xs text-white hover:text-amber-200" title="Administrace">
+        A
+      </div>
+    </NuxtLink>
+  </footer>
+</template>
